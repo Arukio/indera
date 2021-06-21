ce6e6c1794b6004b243e524b89e24011c8728c55
irfan marzuki <irfnmzk@gmail.com>
2021-06-21 21:56:36 +0700

    update constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,18 @@
-// export const RPC_URL = 'https://bsc-dataseed1.defibit.io/'
+export const RPC_URL = 'https://bsc-dataseed1.defibit.io/'
 
-// export const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
-// export const ROUTER_ADDRESS = '0x10ed43c718714eb63d5aa57b78b54704e256024e'
+export const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
+export const ROUTER_ADDRESS = '0x10ed43c718714eb63d5aa57b78b54704e256024e'
 
-// export const WBNB_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-
-// export const MIN_LIQUIDITY = 0.1
-
-// TESTNET
-export const RPC_URL = 'https://data-seed-prebsc-1-s1.binance.org:8545/'
-
-export const FACTORY_ADDRESS = '0x6725f303b657a9451d8ba641348b6761a6cc7a17'
-export const ROUTER_ADDRESS = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1'
-
-export const WBNB_ADDRESS = '0xae13d989dac2f0debff460ac112a837c89baa7cd'
+export const WBNB_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
 
 export const MIN_LIQUIDITY = 0.1
+
+// TESTNET
+// export const RPC_URL = 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+
+// export const FACTORY_ADDRESS = '0x6725f303b657a9451d8ba641348b6761a6cc7a17'
+// export const ROUTER_ADDRESS = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1'
+
+// export const WBNB_ADDRESS = '0xae13d989dac2f0debff460ac112a837c89baa7cd'
+
+// export const MIN_LIQUIDITY = 0.1
